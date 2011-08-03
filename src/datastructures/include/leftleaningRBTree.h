da207bdb0aa14762a4b29494e220f822676fcd43
almost-surely <cappadonza@hotmail.com>
2011-08-03 22:11:22 +0100

    first step in implementing left leaning red black tree

--- src/datastructures/include/leftleaningRBTree.h
+++ src/datastructures/include/leftleaningRBTree.h
@@ -1,23 +1,58 @@
 #include "assert.h"
 #include <functional>
 namespace datastructures {
-  template <typename T_Key, typename T_Item, typename Compare_fn = std::less<T_Key> >  
-    class LLRBTree  {
 
+  
+  
+  template <typename T_Key, typename T_Value, typename Compare_fn = std::less<T_Key> >  
+    class LLRBTree   {
   public:
-  LLRBTree() {};
-    
-
-  void insert(T_Key key, T_Item values){
+  struct Node {
+    T_Key key;
+    T_Value value;
+    bool color;
+    Node *left, *right;
+    Node() { color = !RED; }
+    Node(T_Key k, T_Value v) {
+      key = k;
+      value = v;
+      color = RED;
+    };
+    inline bool is_red() {return color;};
+    inline void set_color_black() { color = !RED;};
+    inline void split_if_4node() { 
+      /*   if(this->left != NULL && this-> right != NULL 
+	 && this->left->is_red() && this->right->is_red()) {
+	this->set_color_red();
+	this->
+	}*/
+    };
   };
 
-  T_Item find(T_Key key) const{
-    T_Item i;
+  typedef Node* Link;
+  
+  LLRBTree() { root = NULL;};
+  
+  void insert(T_Key key, T_Value value){
+    rb_insert(root, key, value);
+    root->set_color_black();
+  };
+
+  T_Value find(T_Key key) const {
+    T_Value i;
     return i;
   };
-
-
   private:
+  static const bool RED = true;
+  Link root;
   Compare_fn _compare_fn;
+
+  void rb_insert(Link& head, T_Key key, T_Value value) { 
+    if(head == NULL) {
+      head = new Node(key, value);
+      return;
+    }
+    head->split_if_4node();
+  }
   };
 }
--- tests/leftleaningRBTree_tests.cc
+++ tests/leftleaningRBTree_tests.cc
@@ -1,7 +1,7 @@
 #include "gtest/gtest.h"
 #include "leftleaningRBTree.h"
 #include <functional>
-
+using namespace datastructures;
 
 class LeftLeaningRBTreeTest : public testing::Test {
  protected:
@@ -17,8 +17,9 @@ class LeftLeaningRBTreeTest : public testing::Test {
 
 
 TEST_F(LeftLeaningRBTreeTest, InsertAndRetrieve) {
-  datastructures::LLRBTree<int, char> rbTree;
-  //ASSERT_EQ(10, heap.top());
-    
+  LLRBTree<int, char> rbTree;
+  rbTree.insert(1, 'a');
+  rbTree.insert(2, 'b');
+  rbTree.insert(3, 'c');
+  ASSERT_EQ('c', rbTree.find(3));
 }
-
