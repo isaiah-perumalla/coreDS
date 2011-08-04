bf58f3e961af24b91a122da8b1a9879f7152fafc
almost-surely <cappadonza@hotmail.com>
2011-08-04 18:40:29 +0100

    working on red black tree insert

--- src/datastructures/include/leftleaningRBTree.h
+++ src/datastructures/include/leftleaningRBTree.h
@@ -2,8 +2,7 @@
 #include <functional>
 namespace datastructures {
 
-  
-  
+    
   template <typename T_Key, typename T_Value, typename Compare_fn = std::less<T_Key> >  
     class LLRBTree   {
   public:
@@ -12,21 +11,26 @@ namespace datastructures {
     T_Value value;
     bool color;
     Node *left, *right;
-    Node() { color = !RED; }
     Node(T_Key k, T_Value v) {
       key = k;
       value = v;
       color = RED;
+      left = right = NULL;
     };
     inline bool is_red() {return color;};
-    inline void set_color_black() { color = !RED;};
+    inline void color_as_black() { color = !RED;};
+    inline void color_as_red() { color = RED;};
     inline void split_if_4node() { 
-      /*   if(this->left != NULL && this-> right != NULL 
-	 && this->left->is_red() && this->right->is_red()) {
-	this->set_color_red();
-	this->
-	}*/
+      if(isRed(this->left) && isRed(this->right)) {
+	this->color_as_red();
+	this->left->color_as_black();
+	this->right->color_as_black();
+	}
     };
+    inline bool has_red_leaning_right() { 
+      return this->right != NULL && this->right->is_red();
+    };
+
   };
 
   typedef Node* Link;
@@ -35,24 +39,45 @@ namespace datastructures {
   
   void insert(T_Key key, T_Value value){
     rb_insert(root, key, value);
-    root->set_color_black();
+    root->color_as_black();
   };
 
   T_Value find(T_Key key) const {
     T_Value i;
     return i;
   };
+
   private:
   static const bool RED = true;
   Link root;
-  Compare_fn _compare_fn;
-
+  Compare_fn compare_fn;
+  
+  inline static bool  isRed(Link& h) { return h != NULL && h->is_red();};
+  
   void rb_insert(Link& head, T_Key key, T_Value value) { 
     if(head == NULL) {
       head = new Node(key, value);
       return;
     }
     head->split_if_4node();
+    int cmp = compare_fn(key, head->key); 
+    if(cmp == 0) { head->value = value;}
+    else if (cmp < 0) rb_insert(head->left, key, value);
+    else rb_insert(head->right, key, value);
+    
+    rebalance(head);
   }
+  inline void rebalance(Link& h) { 
+    if(h->has_red_leaning_right()) rotate_left(h);
+    if(isRed(h->left) && isRed(h->left->left)) rotate_right(h);
+  };
+  
+  inline void rotate_left(Link& h) {
+        
+  }
+  inline void rotate_right(Link& h) {
+    
+  }
+    
   };
 }
